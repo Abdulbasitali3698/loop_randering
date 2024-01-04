e4cdab1844992ec4d2515d746c27987c05cf5f51
Abdulbasitali3698 <43988292+Abdulbasitali3698@users.noreply.github.com>
2024-01-04 18:22:20 +0500

    counter App

--- app/components/counterSection/counterSection.tsx
+++ app/components/counterSection/counterSection.tsx
@@ -1,15 +1,34 @@
 "use client"
+import { useState } from "react";
 
 import Button from "../button/button"
 
 export default function CounterSection(){
-    const onClickHandler=()=>{
-        alert("Onclick handler by counter section")
+    var count:number=0;
+    const[countState,setCountState]=useState(0)
+    const onClickAddHandler=()=>{
+        // alert("Add")
+        count=count+1;
+        setCountState(countState+1)
+        console.log("onClickAddHandler",count)
+    }
+    const onClickMinusHandler=()=>{
+        // alert("Minus")
+        count=count-1;
+        setCountState(countState-1)
+        console.log("onClickMinusHandler",count)
+    }
+
+    const resetHandler=()=>{
+        setCountState(0)
     }
     return(
         <div> 
-            <Button title="Add" onClickHandler={onClickHandler}/>
-
+            <Button title="+" onClickHandler={onClickAddHandler}/>
+            <span>{countState}</span>
+            <Button title="-" onClickHandler={onClickMinusHandler}/>
+            <br />
+            <Button title="Reset" onClickHandler={resetHandler}/>
         </div>
     )
 }
--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -11,6 +11,7 @@ export default function Counter(){
             <CounterSection/>
             
             
+            
         </div>
     )
 }
